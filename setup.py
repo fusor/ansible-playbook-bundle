2b3e5daea4433e3b512d85f146564c6d0dfce6ae
Dylan Murray <dymurray@redhat.com>
2017-08-02 14:15:07 -0400

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="apb",
-    version="0.2.0",
+    version="0.2.1",
     description="Tooling for managing Ansible Playbook Bundle (APB) projects",
     author="Fusor",
     author_email="ansible-service-broker@redhat.com",
