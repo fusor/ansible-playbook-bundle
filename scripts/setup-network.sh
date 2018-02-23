8c1028d520a3604059e20c3b7b2823bbc4cb971b
Erik Nelson <erik@nsk.io>
2018-02-23 16:56:20 -0500

    Parameterize minishift subnet
    * Also add warning telling the caller network-setup must be run as root.

--- scripts/setup-network.sh
+++ scripts/setup-network.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
-iptables -I INPUT -m tcp -p tcp -m multiport --dports 443,8443 -d 192.168.42.0/24 -j ACCEPT
-iptables -I FORWARD -m tcp -p tcp -m multiport --dports 443,8443 -d 192.168.42.0/24 -j ACCEPT
-iptables -I FORWARD -s 192.168.42.0/24 -j ACCEPT
+MINISHIFT_SUBNET=${MINISHIFT_SUBNET:-192.168.42.0/24}
+
+echo "Adding iptables rules for minishift subnet: ${MINISHIFT_SUBNET}"
+
+if ! [[ $(id -u) = 0 ]]; then
+  echo "ERROR: Must run setup-network.sh as root!"
+  exit 1
+fi
+
+iptables -I INPUT -m tcp -p tcp -m multiport --dports 443,8443 -d ${MINISHIFT_SUBNET} -j ACCEPT
+iptables -I FORWARD -m tcp -p tcp -m multiport --dports 443,8443 -d ${MINISHIFT_SUBNET} -j ACCEPT
+iptables -I FORWARD -s ${MINISHIFT_SUBNET} -j ACCEPT
